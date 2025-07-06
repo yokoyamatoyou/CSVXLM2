bfa5206714661c347275797690e5b2662fdd8039
yokoyamatoyou <134029955+yokoyamatoyou@users.noreply.github.com>
2025-07-06 10:32:19 +0900

    Refactor package init

--- src/csv_to_xml_converter/__init__.py
+++ src/csv_to_xml_converter/__init__.py
@@ -9,17 +9,23 @@
 
 import logging
 
+from .models import (
+    II_Element,
+    CD_Element,
+    MO_Element_Data,
+    CDAHeaderData,
+    ObservationDataItem,
+    ObservationGroup,
+    HealthCheckupRecord,
+    HealthGuidanceRecord,
+    CheckupSettlementRecord,
+    GuidanceSettlementRecord,
+)
+
 VERSION = "0.1.0"
 
 logger = logging.getLogger(__name__)
 logger.info("csv_to_xml_converter package version %s initialized.", VERSION)
-
-from .models import (
-    II_Element, CD_Element, MO_Element_Data,
-    CDAHeaderData, ObservationDataItem, ObservationGroup,
-    HealthCheckupRecord, HealthGuidanceRecord,
-    CheckupSettlementRecord, GuidanceSettlementRecord
-)
 
 __all__ = [
     "II_Element",
