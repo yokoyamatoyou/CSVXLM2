9080b07d48e03c927a584bb079580dbc744f0d1d
yokoyamatoyou <134029955+yokoyamatoyou@users.noreply.github.com>
2025-07-23 14:48:19 +0900

    Remove convert_csv_to_json from utils init

--- src/csv_to_xml_converter/utils/__init__.py
+++ src/csv_to_xml_converter/utils/__init__.py
@@ -8,7 +8,6 @@ from typing import Optional, Union
 
 from lxml import etree
 
-from .csv_to_json import convert_csv_to_json
 
 logger = logging.getLogger(__name__)
 
@@ -32,4 +31,4 @@ def parse_xml(path: Union[str, PathLike[str]]) -> Optional[etree._ElementTree]:
     return None
 
 
-__all__ = ["parse_xml", "convert_csv_to_json"]
+__all__ = ["parse_xml"]
